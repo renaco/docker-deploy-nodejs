0308fcd0ddafaad36041d82a66f6d158a7d355df
renaco <renanromero@gmail.com>
2019-01-11 20:06:17 -0500

    chore app: add editor config and update style code

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ let faker = require('faker');
 let PORT=3000;
 
 function handleRequest(request, response){
-    response.end('Hello World!');
+  response.end('Hello World!');
 }
 
 let server = http.createServer(function(request, response){
-    if (request.url === '/') {
-        console.log("Request received. Greeting user!");
-        let user = faker.name.findName();
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.end(`Hello <strong>${user}</strong>!`);
-    }
+  if (request.url === '/') {
+    console.log("Request received. Greeting user!");
+    let user = faker.name.findName();
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end(`Hello <strong>${user}</strong>!`);
+  }
 });
 
 server.listen(PORT, function(){
-    console.log("Server listening on: http://localhost:%s", PORT);
+  console.log("Server listening on: http://localhost:%s", PORT);
 });
